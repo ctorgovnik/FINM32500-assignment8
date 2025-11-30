ca8d94a36e00d3bb7c1bd840b8f5d2bd7c3de240
azakharove <azakharov@uchicago.edu>
2025-11-30 16:13:53 -0600

    added missing config parameter

--- Gateway/run.py
+++ Gateway/run.py
@@ -22,7 +22,7 @@ def run_gateway(config: dict):
         return
     
     md_stream = Stream(market_provider, config["md_port"], config["delimiter"], logger)
-    news_provider = NewsProvider()
+    news_provider = NewsProvider(config = config)
     news_stream = Stream(news_provider, config["news_port"], config["delimiter"], logger)
 
     for stream in [md_stream, news_stream]:
@@ -57,5 +57,6 @@ if __name__ == "__main__":
         "data_path": "data/market_data.csv",
         "md_port": 8000,
         "news_port": 8001,
-        "delimiter": b'*'
+        "delimiter": b'*',
+        "symbols": ["AAPL", "MSFT", "SPY"]
     })
